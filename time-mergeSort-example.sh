dca57dccaf5dc39344b7dc9bc6cd6b0f2268cd1b
Matthew Hammer <matthewhammer@gmail.com>
2026-03-26 14:20:47 -0600

    run-example-mergeSort.sh in GH actions.

--- time-mergeSort-example.sh
+++
@@ -1,7 +0,0 @@
-{
-    time \
-    stdbuf -oL -eL \
-    cargo run --release -- \
-    eval "import M \"fumola/examples/mergeSort/mergeSort\"; M.run()" \
-    --import $(find fumola -name "*.fumola")
-} 2>&1 | tee time-mergeSort-example.output
