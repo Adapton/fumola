0407eb2f0ea746d995847f6b6b86f461b22c6ec5
Matthew Hammer <matthewhammer@gmail.com>
2026-04-08 10:13:57 -0600

    document hashMap prims, module API with basic unit test for each prim.

--- crates/fumola/tests/test_adapton.rs
+++ crates/fumola/tests/test_adapton.rs
@@ -253,25 +253,3 @@ assert ((@ cell) == ?1)
         "()",
     )
 }
-
-#[test]
-fn test_events() {
-    assert__(
-        r#"
-    force (1 := thunk { force (2 := thunk { 44 }) });
-    (switch (@(`adapton(`state))) { case (#Graphical(g)) g }).events"#,
-        r#"
-    [
-        {event = #AddNode(#Symbol(#Nat(1)), #Now, [1]); meta_time = [1]},
-        {event = #AddEdge([1001]); meta_time = [1]},
-        {event = #ForceBegin(#Symbol(#Nat(1)), #Now, [1]); meta_time = [2]},
-        {event = #AddNode(#Symbol(#Nat(2)), #Now, [3]); meta_time = [3]},
-        {event = #AddEdge([1002]); meta_time = [3]},
-        {event = #ForceBegin(#Symbol(#Nat(2)), #Now, [3]); meta_time = [4]},
-        {event = #AddEdge([1003]); meta_time = [5]},
-        {event = #ForceEnd((#Symbol(#Nat(2)), #Now, [3]), [1003]); meta_time = [5]},
-        {event = #AddEdge([1004]); meta_time = [6]},
-        {event = #ForceEnd((#Symbol(#Nat(1)), #Now, [1]), [1004]); meta_time = [6]}
-    ]"#,
-    );
-}
