bc1040e0c79e7d60b8e827441a6c94444508b0d5
Matthew Hammer <matthewhammer@gmail.com>
2026-07-20 08:11:21 -0600

    44 scene objects instead of 23. fixing glitches in replay animation.

--- write-merge-sort-scene-larger.sh
+++ write-merge-sort-scene-larger.sh
@@ -2,6 +2,6 @@
     time \
     stdbuf -oL -eL \
     cargo run  -- \
-    eval 'import M "fumola/examples/mergeSort/mergeSort"; M.generateSceneFullDemand(10, 23)' \
+    eval 'import M "fumola/examples/mergeSort/mergeSort"; M.generateSceneFullDemand(10, 44)' \
     --import $(find fumola -name "*.fumola")
 } 2>&1 | tee write-merge-sort-scene.output
--- write-merge-sort-scene.sh
+++ write-merge-sort-scene.sh
@@ -2,6 +2,6 @@
     time \
     stdbuf -oL -eL \
     cargo run  -- \
-    eval 'import M "fumola/examples/mergeSort/mergeSort"; M.testGenerateSceneFullDemand()' \
+    eval 'import M "fumola/examples/mergeSort/mergeSort"; M.generateSceneFullDemand(10, 4)' \
     --import $(find fumola -name "*.fumola")
 } 2>&1 | tee write-merge-sort-scene.output
