73c880136b0fbe3bd5e4ad3be7510979c2a9c106
Matthew Hammer <matthewhammer@gmail.com>
2026-07-22 06:25:35 -0600

    fix test

--- write-merge-sort-scene-larger.sh
+++ write-merge-sort-scene-larger.sh
@@ -2,6 +2,6 @@
     time \
     stdbuf -oL -eL \
     cargo run  -- \
-    eval 'import M "fumola/examples/mergeSort/mergeSort"; M.generateSceneFullDemand(10, 44)' \
+    eval 'import M "fumola/examples/mergeSort/mergeSort"; let out = M.generateSceneFullDemand(10, 23); out.sceneData.objects.size()' \
     --import $(find fumola -name "*.fumola")
 } 2>&1 | tee write-merge-sort-scene.output
