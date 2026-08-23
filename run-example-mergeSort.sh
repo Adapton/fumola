0039f111f6be1ea051d7afed3fe43316a9c1fc3c
Matthew Hammer <matthew.hammer@gmail.com>
2026-08-23 13:58:53 -0600

    Refactor `mergeSort` again. (#40)
    * fix scene.json file, post merge.
    
    * fix script; fix mergeSort edit placement; elim dead/extra code.
    
    * CI -- faster tests by using release everywhere, despite extra compilation costs.
    
    * git.
    
    * thoughts for later.
    
    * fix test after removing some components that never had 3d components.
    
    ---------
    
    Co-authored-by: Matthew Hammer <matthewhammer@gmail.com>

--- run-example-mergeSort.sh
+++ run-example-mergeSort.sh
@@ -1,7 +1,7 @@
 {
     time \
     stdbuf -oL -eL \
-    cargo run  -- \
+    cargo run --release -- \
     eval "import M \"fumola/examples/mergeSort/mergeSort\"; M.runAll()" \
     --import $(find fumola -name "*.fumola")
 } 2>&1 | tee mergeSort-example.output
