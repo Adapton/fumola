ceb7de48564d46ce11ab26e7c8224c8dc8a91d07
Matthew Hammer <matthew.hammer@gmail.com>
2026-08-23 14:53:31 -0600

    Update canonical `scene.json` file for `mergeSort`. (#42)
    * update scene file with new metaTimes and edgeIds; no objects are repositioned.
    
    * fix typo
    
    ---------
    
    Co-authored-by: Matthew Hammer <matthewhammer@gmail.com>

--- write-merge-sort-scene.sh
+++ write-merge-sort-scene.sh
@@ -2,6 +2,6 @@
     time \
     stdbuf -oL -eL \
     cargo run  -- \
-    eval 'import M "fumola/examples/mergeSort/mergeSort"; let output = M.generateSceneFullDemand(10, 4); output.sceneData.objects.size()' \
+    eval 'import M "fumola/examples/mergeSort/mergeSort"; let output = M.generateSceneFullDemand(10, 4, null); output.sceneData.objects.size()' \
     --import $(find fumola -name "*.fumola")
 } 2>&1 | tee write-merge-sort-scene.output
